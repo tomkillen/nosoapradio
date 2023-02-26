147c49574426ff858c50c7195bd9bd10c1313072
Tom Killen <tomkillen@gmail.com>
2023-02-26 21:11:09 +0100

    Created a boring radio station list to test out the radio station data provider

---
+++ bubbleradioapp/lib/widgets/radio_station_list.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../models/radio_station.dart';
+import '../services/radio_stations_service.dart';
+
+class RadioStationList extends StatelessWidget {
+  const RadioStationList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<RadioStationsService, List<RadioStation>>(
+      builder: (context, stations) {
+        return ListView.builder(
+          itemCount: stations.length,
+          itemBuilder: (context, index) {
+            final station = stations[index];
+            return ListTile(
+              title: Text(station.name),
+              subtitle: Text(station.url),
+            );
+          },
+        );
+      },
+      buildWhen: (previous, current) => current.isNotEmpty,
+      bloc: BlocProvider.of<RadioStationsService>(context)..getRadioStations(),
+    );
+  }
+}
