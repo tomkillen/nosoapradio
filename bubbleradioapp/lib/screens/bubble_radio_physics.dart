5087c506b5c30d66b29ab6c98b03501a00b38ce6
Tom Killen <tomkillen@gmail.com>
2023-02-26 21:17:08 +0100

    bubble simulation is working with gravity and colliders, with a simple debug renderer for the moment not yet radio widgets

--- bubbleradioapp/lib/screens/bubble_radio_physics.dart
+++ bubbleradioapp/lib/screens/bubble_radio_physics.dart
@@ -1,126 +1,260 @@
-import 'dart:math';
 import 'dart:async' as dart_async;
+import 'dart:math';
+import 'dart:ui';
+import 'package:flutter/scheduler.dart';
 import 'package:forge2d/forge2d.dart';
 import 'package:flutter/material.dart';
 
-class BubbleSimulationWidget extends StatefulWidget {
-  final BubblesSimulation bubblesSimulation =
-      BubblesSimulation(size: const Size(500, 500));
+class BubbleSimulationWidget extends LeafRenderObjectWidget {
+  final BubbleSimulation bubbleSimulation = BubbleSimulation();
 
-  BubbleSimulationWidget({super.key});
-
-  @override
-  State<BubbleSimulationWidget> createState() => _BubbleSimulationWidgetState();
-}
-
-class _BubbleSimulationWidgetState extends State<BubbleSimulationWidget> {
-  @override
-  void initState() {
-    super.initState();
+  BubbleSimulationWidget({super.key}) {
+    final Size simSize = window.physicalSize;
+    bubbleSimulation.initialize(simSize, Vector2(0, 0));
+    bubbleSimulation.spawnBubbles(1);
   }
 
   @override
-  Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: BubblesPainter(bubblesSimulation: widget.bubblesSimulation),
-      size: MediaQuery.of(context).size,
-    );
+  RenderObject createRenderObject(BuildContext context) {
+    return BubbleSimulationRenderObject(bubbleSimulation: bubbleSimulation);
   }
+
+  // @override
+  // State<BubbleSimulationWidget> createState() => _BubbleSimulationWidgetState();
 }
 
-class BubblesPainter extends CustomPainter {
-  final BubblesSimulation bubblesSimulation;
+// class _BubbleSimulationWidgetState extends State<BubbleSimulationWidget> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     final Size simSize = window.physicalSize;
+//     widget.bubbleSimulation.initialize(simSize, Vector2(0, 9.8));
+//     widget.bubbleSimulation.spawnBubbles(3);
+//   }
 
-  BubblesPainter({required this.bubblesSimulation});
+//   @override
+//   Widget build(BuildContext context) {
+//     return BubbleSimulationRenderObject(
+//         bubbleSimulation: widget.bubbleSimulation);
+//     // return CustomPaint(
+//     //   painter: BubblesPainter(bubblesSimulation: widget.bubbleSimulation),
+//     //   size: MediaQuery.of(context).size,
+//     //   willChange: true,
+//     // );
+//   }
+// }
+
+// class BubblesPainter extends CustomPainter {
+//   final BubbleSimulation bubblesSimulation;
+
+//   BubblesPainter({required this.bubblesSimulation});
+
+//   @override
+//   void paint(Canvas canvas, Size size) {
+//     for (var bubble in bubblesSimulation.bubbles) {
+//       print('Drawing bubble: ${bubble.color}');
+//       canvas.drawCircle(
+//           bubble.offset, bubble.radius, Paint()..color = bubble.color);
+//     }
+//   }
+
+//   @override
+//   bool shouldRepaint(BubblesPainter oldDelegate) {
+//     return true;
+//   }
+// }
+
+class BubbleSimulationRenderObject extends RenderBox {
+  final BubbleSimulation bubbleSimulation;
+
+  BubbleSimulationRenderObject({required this.bubbleSimulation}) {
+    bubbleSimulation.onNeedPaint = markNeedsPaint;
+  }
 
   @override
-  void paint(Canvas canvas, Size size) {
-    for (var bubbleBody in bubblesSimulation.physics.bubbleBodys) {
-      Bubble bubble = bubbleBody.userData as Bubble;
-      canvas.drawCircle(Offset(bubbleBody.position.x, bubbleBody.position.y),
-          bubble.radius, Paint()..color = bubble.color);
+  bool get isRepaintBoundary => true;
+
+  @override
+  bool get sizedByParent => true;
+
+  @override
+  Size computeDryLayout(BoxConstraints constraints) {
+    return constraints.biggest;
+  }
+
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    _drawBubbles(context, offset);
+  }
+
+  void _drawBubbles(PaintingContext context, Offset offset) async {
+    Canvas canvas = context.canvas;
+    var rect = Rect.fromCenter(
+        center: bubbleSimulation.offset,
+        width: bubbleSimulation.size.width,
+        height: bubbleSimulation.size.height);
+    canvas.save();
+    canvas.clipPath(Path()..addRect(rect));
+
+    for (var body in bubbleSimulation._world.bodies) {
+      Bubble? bubble = body.userData as Bubble?;
+      if (bubble != null) {
+        _drawBubble(canvas, bubble, body);
+      }
     }
+
+    canvas.restore();
   }
 
-  @override
-  bool shouldRepaint(BubblesPainter oldDelegate) {
-    return true;
+  void _drawBubble(Canvas canvas, Bubble bubble, Body body) {
+    double angle = body.angle;
+    Vector2 position = body.position * BubbleSimulation.ppm;
+    print('Drawing bubble at $position $angle');
+    Matrix4 matrix = Matrix4.identity()
+      ..leftTranslate(position.x / 2, position.y / 2)
+      ..rotateZ(angle);
+    canvas.save();
+    canvas.transform(matrix.storage);
+    for (Fixture f in body.fixtures) {
+      if (f.type == ShapeType.circle) {
+        _drawCircleShape(canvas, f.shape as CircleShape, bubble.color);
+      }
+    }
+    canvas.restore();
+  }
+
+  void _drawCircleShape(Canvas canvas, CircleShape circle, Color color) {
+    // canvas.drawCircle(
+    //     Offset(circle.position.x * BubbleSimulation.ppm,
+    //         circle.position.y * BubbleSimulation.ppm),
+    //     circle.radius * BubbleSimulation.ppm,
+    //     Paint()
+    //       ..style = PaintingStyle.fill
+    //       ..color = color);
+    canvas.drawCircle(
+        Offset.zero,
+        50,
+        Paint()
+          ..style = PaintingStyle.fill
+          ..color = color);
   }
 }
 
 typedef PaintCallback = Function();
 
-class BubblesSimulation {
-  final BubblesPhysics physics;
+class BubbleSimulation {
+  // pixels per meter / pixels per unit of the sim
+  static const double ppm = 100;
 
-  BubblesSimulation({required Size size})
-      : physics = BubblesPhysics(
-            size: size, gravity: Vector2(0, 9.8), maxNumBubbles: 5);
-}
-
-class BubblesPhysics {
-  final Size size;
-  final Vector2 origin;
-  final World world;
-  final int maxNumBubbles;
-  final double minBubbleRadius = 32;
-  final double maxBubbleRadius = 64;
-  late final double bubbleRadiusVariance;
-  final double ppm = 100;
-  Vector2 gravity;
-  List<Body> bubbleBodys = [];
+  final List<Bubble> bubbles = [];
+  PaintCallback? onNeedPaint;
 
   final _random = Random();
+  final World _world = World();
+  Vector2 _origin = Vector2.zero();
+  Size _size = Size.zero;
+  Offset _offset = Offset.zero;
+  bool _initialized = false;
+  bool _running = false;
+  int? _frameCallbackId;
 
   // Debug colors for rendering bubbles before I have album art loading
-  final List<Color> colors = [
+  final List<Color> _colors = [
     const Color.fromARGB(255, 62, 36, 251),
     const Color.fromARGB(255, 39, 230, 227),
     const Color.fromARGB(255, 253, 112, 33),
     const Color.fromARGB(255, 253, 66, 193),
   ];
 
-  BubblesPhysics(
-      {required this.size, required this.gravity, required this.maxNumBubbles})
-      : world = World(gravity),
-        origin = Vector2(size.width / 2, size.height / 2) {
-    if (minBubbleRadius < 1 || maxBubbleRadius < minBubbleRadius) {
-      throw ArgumentError(
-          'Bubble radius must satisty minBubbleRadius >= 1 >= maxBubbleRadius, and we got $minBubbleRadius >= 1 >= $maxBubbleRadius');
-    }
-    bubbleRadiusVariance = maxBubbleRadius - minBubbleRadius;
+  Vector2 get origin => _origin;
+  Size get size => _size;
+  Offset get offset => _offset;
+
+  BubbleSimulation() {
     // forge2d settings
     velocityIterations = 5;
     positionIterations = 5;
   }
 
-  void spawnBubbles(int numBubblesToSpawn) {
-    // ensure we do not spawn more bubbles than we can spawn
-    numBubblesToSpawn =
-        min(numBubblesToSpawn, maxNumBubbles - bubbleBodys.length);
-    if (numBubblesToSpawn > 0) {
-      // Spawn in the desired bubbles with a brief delay per bubble
-      dart_async.Timer.periodic(const Duration(milliseconds: 70), (timer) {
-        double spawnX = _random.nextDouble() * origin.x + origin.x;
-        _spawnBubble(Vector2(spawnX, origin.y),
-            _random.nextDouble() * bubbleRadiusVariance + minBubbleRadius);
-        // cancel the timer when we have spawned all of our bubbles
-        numBubblesToSpawn -= 1;
-        if (numBubblesToSpawn <= 0) {
-          timer.cancel();
-        }
-      });
+  void initialize(Size size, Vector2 gravity) {
+    _initialized = true;
+    _size = size;
+    _origin = Vector2(size.width / 2, size.height / 2);
+    _offset = Offset(_origin.x, _origin.y);
+    _world.setGravity(gravity);
+    _running = true;
+    _frameCallbackId =
+        SchedulerBinding.instance.scheduleFrameCallback(_frameCallback);
+  }
+
+  void pause() {
+    if (_running) {
+      if (_frameCallbackId != null) {
+        SchedulerBinding.instance.cancelFrameCallbackWithId(_frameCallbackId!);
+        _frameCallbackId = null;
+      }
+      _running = false;
     }
   }
 
-  void step(double stepDeltaSeconds) {
-    world.stepDt(stepDeltaSeconds);
+  void resume() {
+    if (_initialized && !_running) {
+      _running = true;
+      _frameCallbackId =
+          SchedulerBinding.instance.scheduleFrameCallback(_frameCallback);
+    }
   }
 
-  void _spawnBubble(Vector2 position, double ballRadius) {
-    final bouncingRectangle = CircleShape()..radius = ballRadius / ppm;
+  void spawnBubbles(int numBubblesToSpawn) {
+    if (!_initialized) {
+      throw Exception('Bubble simulation not yet initialized');
+    }
 
-    final activeFixtureDef = FixtureDef(bouncingRectangle)
+    // Spawn in the desired bubbles with a brief delay per bubble
+    dart_async.Timer.periodic(const Duration(milliseconds: 70), (timer) {
+      double spawnX = _random.nextDouble() * _origin.x + _origin.x;
+      _spawnBubble(Vector2(spawnX, _origin.y), _random.nextDouble() * 32 + 32);
+      // cancel the timer when we have spawned all of our bubbles
+      numBubblesToSpawn -= 1;
+      if (numBubblesToSpawn <= 0) {
+        timer.cancel();
+      }
+    });
+  }
+
+  void _frameCallback(Duration frameDelta) {
+    // Calculate frame delta seconds based by converting the duration
+    // milliseconds for highest available accuracy
+    final double deltaTimeSeconds = frameDelta.inMicroseconds /
+        Duration.microsecondsPerMillisecond /
+        1000.0;
+
+    // Tick the simulation
+    _step(deltaTimeSeconds);
+
+    // Mark we need a repaint
+    onNeedPaint?.call();
+
+    // Schedule the next frame update
+    if (_running) {
+      _frameCallbackId = SchedulerBinding.instance
+          .scheduleFrameCallback(_frameCallback, rescheduling: true);
+    }
+  }
+
+  void _step(double stepDeltaSeconds) {
+    assert(_initialized);
+    _world.stepDt(stepDeltaSeconds);
+  }
+
+  void _spawnBubble(Vector2 position, double radius) {
+    assert(_initialized);
+
+    final color = _colors[_random.nextInt(_colors.length)];
+    final bubble = Bubble(color: color, radius: radius);
+    final circleShape = CircleShape()..radius = radius / ppm;
+
+    final activeFixtureDef = FixtureDef(circleShape)
       ..density = 4
       ..friction = 1
       ..restitution = .2;
@@ -129,12 +263,11 @@ class BubblesPhysics {
     activeBodyDef.position = position / ppm;
     activeBodyDef.type = BodyType.dynamic;
     activeBodyDef.bullet = true;
-    Body boxBody = world.createBody(activeBodyDef);
-    boxBody.createFixture(activeFixtureDef);
-    boxBody.userData = Bubble(
-        color: colors[_random.nextInt(colors.length)], radius: ballRadius);
+    activeBodyDef.userData = bubble;
+    Body body = _world.createBody(activeBodyDef);
+    body.createFixture(activeFixtureDef);
 
-    bubbleBodys.add(boxBody);
+    bubbles.add(bubble);
   }
 }
 
